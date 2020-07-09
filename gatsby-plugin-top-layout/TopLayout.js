caa63a86efceba993948c9864137cb0b92d7876c
Ana Garcia <agarcia-caicedo@myseneca.ca>
2020-07-08 20:05:46 -0400

    moved plugin folder

--- gatsby-plugin-top-layout/TopLayout.js
+++ gatsby-plugin-top-layout/TopLayout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
-import theme from '../../theme';
+import theme from '../src/theme';
 
 export default function TopLayout(props) {
   return (
